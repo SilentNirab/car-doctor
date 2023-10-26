2dd61e79d4164f9f7a5b0238e316ac1387354429
Md Nirab <nabilnirab255@gmail.com>
2023-10-26 23:39:52 +0600

    Login Page added

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from '../Root/Root';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
+import SignUp from '../Pages/SginUp/SignUp';
 
 const routers = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const routers = createBrowserRouter([
             {
                 path: "/login",
                 element: <Login></Login>
+            },
+            {
+                path: "/signup",
+                element: <SignUp></SignUp>
             }
         ]
     }
